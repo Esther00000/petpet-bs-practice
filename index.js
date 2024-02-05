d2a3f0d3216e757d929cd5e21f32ac7a84dadc5d
Esther <fuest8456258@gmail.com>
2024-02-05 21:44:19 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Params
+const mainSliderSelector = ".main-slider";
+const thumbSliderSelector = ".thumb-slider";
+
 const swiper = new Swiper(".nannySwiper", {
   loop: true,
   slidesPerView: 2,
@@ -12,9 +18,6 @@ const swiper = new Swiper(".nannySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-// Params
-const mainSliderSelector = ".main-slider";
-const thumbSliderSelector = ".thumb-slider";
 // Main Slider
 const thumbSlider = new Swiper(thumbSliderSelector, {
   slidesPerView: 4,
@@ -25,45 +28,3 @@ const mainSlider = new Swiper(mainSliderSelector, {
     swiper: thumbSlider,
   },
 });
-
-// service page
-const serviceStart = document.querySelector("#start");
-const serviceEnd = document.querySelector("#end");
-
-function datePicker(targetStart, targetEnd) {
-  const startDate = flatpickr(targetStart, {
-    enableTime: false,
-    dateFormat: "Y-m-d", // 設定日期格式為 '年-月-日'
-    defaultDate: "today", // 預設日期為今天
-    minDate: "today", // 設定最小日期為今天
-    // 當開始日期改變時，更新結束日期的最小允許日期
-    onChange: function (selectedDates, dateStr, instance) {
-      // 取得開始日期的後一天
-      const nextDay = new Date(dateStr);
-      nextDay.setDate(nextDay.getDate() + 1);
-      flatpickr("#end", {
-        minDate: nextDay, // 結束日期的最小允許日期，dateStr : 選擇開始日期的日期
-        defaultDate: nextDay,
-      });
-    },
-  });
-  const tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  const endDate = flatpickr(targetEnd, {
-    enableTime: false, // 不啟用時間選擇
-    dateFormat: "Y-m-d", // 設定日期格式為 '年-月-日'
-    defaultDate: tomorrow, // 預設日期為明天
-    minDate: tomorrow, // 設定最小日期為明天
-  });
-}
-const reserveModal = document.getElementById("reserveModa1");
-
-reserveModal.addEventListener("shown.bs.modal", () => {
-  // detail page
-  const detailStart = document.querySelector("#detailStart");
-  const detailEnd = document.querySelector("#detailEnd");
-  console.log(detailStart, detailEnd);
-  datePicker(detailStart, detailEnd);
-});
-
-datePicker(serviceStart, serviceEnd);
